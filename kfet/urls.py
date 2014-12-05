6eaa7da417d51b519bc7668239cf9c368800f5b8
faerie <faerie@MamaFaerie>
2014-12-05 01:15:40 +0100

    Almost finished kfet module. The last bug to resolve is that you have to click on the item to access the page. Bad.

--- kfet/urls.py
+++ kfet/urls.py
@@ -4,5 +4,6 @@ urlpatterns = patterns('kfet.views',
 		url(r'^$', "index", name = 'index_kfet'),
 		url(r'^products/$', "products", name = 'products'),
 		url(r'^statistics/$', "statistics", name = 'statistics'),
-		url(r'^print_pg/$', "print_pg", name = 'pg'),
+		url(r'^getPg/', "getPg", name = 'getPg'),
+		url(r'^getPgs/', "getPgs", name = "getPgs"),
 )
